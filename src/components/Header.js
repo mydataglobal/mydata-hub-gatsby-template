a377eedef2de79723feb84cb05a23c1559943c71
wip-abramson <wip.abramson@gmail.com>
2019-05-15 03:55:26 +0100

    initial template from code bushi starter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/mydata/mydata-wheel-logo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <h1>MyData Scotland</h1>
+        <p>A Scottish community of the data conscious individuals<br />
+        part of the  <a href="https://mydata.org">MyData Global</a> organisation.</p>
     </header>
 )
 
